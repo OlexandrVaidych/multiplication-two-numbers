322e47253f9fb59453b03e27919c6f32baf0a40b
unknown <10lexandr.vaidych@gmail.com>
2021-10-20 17:16:06 +0300

    Added multiplication of two numbers

--- MultiplicationTwoNumbers/Program.cs
+++ MultiplicationTwoNumbers/Program.cs
@@ -7,10 +7,13 @@ namespace MultiplicationTwoNumbers
         static void Main(string[] args)
         {
             bool condition = true;
+            int num1 = 0;
+            int num2 = 0;
+            int res;
             while (condition)
             {
                 Console.Write("Enter the first number: ");
-                int num1 = Convert.ToInt32(Console.ReadLine());
+                num1 = Convert.ToInt32(Console.ReadLine());
                 if ( (num1 < 0) || (num1 > 10))
                 {
                     Console.WriteLine($"The number {num1} is not valid! Please enter a number between [0; 10]!");
@@ -20,6 +23,25 @@ namespace MultiplicationTwoNumbers
                     break;
                 }
             }
+
+            while (condition)
+            {
+                Console.Write("Enter the second number: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                if ((num2 < 0) || (num2 > 10))
+                {
+                    Console.WriteLine($"The number {num2} is not valid! Please enter a number between [0; 10]!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            res = num1 * num2;
+            
+            Console.Write($"The result of multiplying {num1} by {num2}: {res}");
+            
             Console.ReadKey();
         }
     }
