93c6ad461dfc93e11e150d6c41e1b2e22e9f9786
unknown <10lexandr.vaidych@gmail.com>
2021-10-20 13:11:31 +0300

    Added input of the first number

--- MultiplicationTwoNumbers/Program.cs
+++ MultiplicationTwoNumbers/Program.cs
@@ -6,7 +6,21 @@ namespace MultiplicationTwoNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool condition = true;
+            while (condition)
+            {
+                Console.Write("Enter the first number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                if ( (num1 < 0) || (num1 > 10))
+                {
+                    Console.WriteLine($"The number {num1} is not valid! Please enter a number between [0; 10]!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
