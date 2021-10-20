142b70132c2ce31226b28847bccfe0de9b8e9077
unknown <10lexandr.vaidych@gmail.com>
2021-10-20 12:28:58 +0300

    Created project

---
+++ MultiplicationTwoNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiplicationTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
