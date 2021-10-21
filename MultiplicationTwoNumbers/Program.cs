27e72154f4def304a966c36a55da1f8a23e8bc23
unknown <10lexandr.vaidych@gmail.com>
2021-10-21 11:47:50 +0300

    Created one while instead of two whiles

--- MultiplicationTwoNumbers/Program.cs
+++ MultiplicationTwoNumbers/Program.cs
@@ -6,41 +6,34 @@ namespace MultiplicationTwoNumbers
     {
         static void Main(string[] args)
         {
-            bool condition = true;
-            int num1 = 0;
-            int num2 = 0;
-            int res;
-            while (condition)
+            int num1, num2, res;
+            bool cond = true;
+            
+            Console.WriteLine("Please enter a number between[0; 10]!");
+            
+            while (cond)
             {
                 Console.Write("Enter the first number: ");
                 num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter the second number: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
                 if ( (num1 < 0) || (num1 > 10))
                 {
                     Console.WriteLine($"The number {num1} is not valid! Please enter a number between [0; 10]!");
                 }
-                else
-                {
-                    break;
-                }
-            }
-
-            while (condition)
-            {
-                Console.Write("Enter the second number: ");
-                num2 = Convert.ToInt32(Console.ReadLine());
-                if ((num2 < 0) || (num2 > 10))
+                else if ((num2 < 0) || (num2 > 10))
                 {
                     Console.WriteLine($"The number {num2} is not valid! Please enter a number between [0; 10]!");
                 }
                 else
                 {
+                    res = num1 * num2;
+                    Console.Write($"The result of multiplication {num1} by {num2}: {res}");
                     break;
                 }
             }
-
-            res = num1 * num2;
-            
-            Console.Write($"The result of multiplying {num1} by {num2}: {res}");
             
             Console.ReadKey();
         }
